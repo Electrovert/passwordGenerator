92e2697fa45b40dd126a80d2aed6d7e67e20067d
Amanda Foster <mandolinfoster@gmail.com>
2019-10-09 21:42:26 -0400

    Added HTML file and contents. Added JavaScript and CSS files, no content

---
+++ script.js
@@ -0,0 +1,12 @@
+confirm 
+
+function generatePassword() {
+    var length = 10,
+    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/)*&^%$#@!"
+    retVal = "";
+    for (var i = 0, n = charset.length; i < length; i++ {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
