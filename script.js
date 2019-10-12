35ea2c1e5626bec131290a8df798e2dd0c567608
Amanda Foster <mandolinfoster@gmail.com>
2019-10-12 17:08:35 -0400

    Added reset.css, background image, image itself within images folder

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function genPassword() {
-    var inputbox = document.getElementById("password");
-    var results = " ";
-    for (var i = 0; i < 10; i++);
+var numAmount = prompt("How many characters would you like your password to be?");
+var speChar = confirm("Would you like to add special characters?");
+var numConfirm = confirm("Would you like to include numbers?");
 
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-    var random = Math.floor(Math.random * 40());
-    var result = chars.charAt(random);
+
+var char = "abcdefghijklmnopqrstuvwxyzAMBDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*()+";
+
+var numAmount = document.getElementById("numAmount");
+var speChar = document.getElementById("speChar");
+var numConfirm = document.getElementById("numConfirm");
+var yourPw = document.getElementById("yourPw");
+
+function password(numAmount, speChar, numConfirm){
+    var pwd = " ";
+    for (var i = 0; i < 129; i++){
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
 }
-
